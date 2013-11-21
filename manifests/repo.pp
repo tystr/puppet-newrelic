bc6c29cbc5cb535adbcb5ff1c532cb73c34a2175
Tyler Stroud <tyler@tylerstroud.com>
2013-11-21 02:42:02 +0000

    Manage repo via yumrepo resource type

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -1,6 +1,16 @@
 class newrelic::repo {
-    exec { 'newrelic-repo':
-        command => 'rpm -Uvh --replacepkgs https://yum.newrelic.com/pub/newrelic/el5/x86_64/newrelic-repo-5-3.noarch.rpm',
-        path => ['/bin', '/usr/bin']
+    file { '/etc/pki/rpm-gpg/RPM-GPG-KEY-NewRelic':
+        source => 'puppet:///modules/newrelic/etc/pki/rpm-gpg/RPM-GPG-KEY-NewRelic',
+        owner => 'root',
+        group => 'root',
+        mode => '644',
     }
+    yumrepo { 'newrelic':
+        baseurl => 'http://yum.newrelic.com/pub/newrelic/el5/$basearch',
+        descr => 'New Relic packages for Enterprise Linux 5 - $basearch',
+        enabled => 1,
+        gpgcheck => 1,
+        gpgkey => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-NewRelic',
+        require => File['/etc/pki/rpm-gpg/RPM-GPG-KEY-NewRelic']
+   }
 }
