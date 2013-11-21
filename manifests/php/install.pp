d8511b2eaf163096f6115ae4385f662260fc3f22
Tyler Stroud <tyler@tylerstroud.com>
2013-11-21 02:40:52 +0000

    Only run newrelic installer if the init script doesn't exist

--- manifests/php/install.pp
+++ manifests/php/install.pp
@@ -10,6 +10,7 @@ class newrelic::php::install ($license_key) {
         command => "newrelic-install install",
         path => ['/bin', '/usr/bin'],
         require => Package['newrelic-php5'],
-        notify => Service['php-fpm']
+        notify => Service['php-fpm'],
+        unless => 'test -f /etc/init.d/newrelic-daemon'
     }
 }
