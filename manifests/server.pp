66ff062f4e4fa60e3631d45f9d7a84238c48650a
Tyler Stroud <tyler@tylerstroud.com>
2013-12-03 00:52:48 +0000

    Tweaked chaining in newrelic module

--- manifests/server.pp
+++ manifests/server.pp
@@ -2,7 +2,7 @@ class newrelic::server ($license_key) {
     class { 'newrelic::server::install': } ->
     class { 'newrelic::server::configure': 
         license_key => $license_key
-    } ->
+    } ~>
     class { 'newrelic::server::service': } ->
     Class['newrelic::server']
 }
