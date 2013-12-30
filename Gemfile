f503af47b79438611fa0cf2be09ae48ac0113918
Tyler Stroud <tyler@tylerstroud.com>
2013-12-29 20:40:03 -0800

    Implement puppetlabs_spec_helper

--- Gemfile
+++ Gemfile
@@ -4,5 +4,6 @@ group :rake do
   gem 'puppet', '>=2.7.0'
   gem 'rspec-puppet', '>=1.0.1'
   gem 'rake', '>=0.9.2.2'
+  gem 'puppetlabs_spec_helper', '>=0.4.0'
   # gem 'puppet-lint', '>=0.1.12'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,2 @@
-dir = File.expand_path(File.dirname(__FILE__))
-$LOAD_PATH.unshift File.join(dir, 'lib')
-
-require 'puppet'
-require 'rspec-puppet'
-#require 'spec/autorun'
-
-fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
-RSpec.configure do |config|
-    config.module_path = File.join(fixture_path, 'modules')
-    config.manifest_dir = File.join(fixture_path, 'manifests')
-end
-
-# We need this because the RAL uses 'should' as a method.  This
-# allows us the same behaviour but with a different method name.
-#class Object
-#    alias :must :should
-#end
+require 'puppetlabs_spec_helper/module_spec_helper'
+require 'puppetlabs_spec_helper/puppetlabs_spec_helper'
