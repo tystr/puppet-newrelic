b5b567f20d7e17edca705d73a9817d1ce2484ad1
andrewmitchell21 <andrew.mitchell@bedegaming.com>
2018-05-03 11:57:51 +0100

    updating module

--- manifests/server.pp
+++ manifests/server.pp
@@ -6,13 +6,13 @@ class newrelic::server (
         license_key => $license_key,
         ensure      => $ensure
     } ->
-    class { 'newrelic::server::service':
-        ensure      => $ensure
-    } ->
     class { 'newrelic::server::configure':
         license_key => $license_key,
         ensure      => $ensure
 
+    } ->
+    class { 'newrelic::server::service':
+        ensure      => $ensure
     } ->
     Class['newrelic::server']
 }
--- manifests/server/install.pp
+++ manifests/server/install.pp
@@ -1,22 +1,8 @@
-class newrelic::server::install (
-    $license_key,
-    $ensure,
-    ) {
+class newrelic::server::install ($ensure) {
     include newrelic::repo
 
     package {'newrelic-sysmond':
         ensure  => $::newrelic::server::install::ensure,
         require => Class['newrelic::repo']
-    }
-
-    if $::newrelic::server::install::ensure != 'absent' {
-        exec { 'nrsysmond-config-license':
-            command     => "nrsysmond-config --set license_key=${license_key}",
-            path        => ['/bin', '/usr/bin', '/usr/sbin'],
-            notify      => Service['newrelic-sysmond'],
-            refreshonly => true,
-            subscripe   => Package['newrelic-sysmod']
-        }
-        Package['newrelic-sysmond'] -> Exec['nrsysmond-config-license']
     }
 }
