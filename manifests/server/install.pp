e420a7a611bd5e2430f54af17fb114ed7c16c307
andrewmitchell21 <andrew.mitchell@bedegaming.com>
2018-05-03 16:26:46 +0100

    latest

--- manifests/server/install.pp
+++ manifests/server/install.pp
@@ -1,9 +1,21 @@
-class newrelic::server::install ($ensure) {
+class newrelic::server::install (
+    $license_key,
+    $ensure,
+    ) {
     include newrelic::repo
 
     package {'newrelic-sysmond':
         ensure  => $::newrelic::server::install::ensure,
         require => Class['newrelic::repo']
-        notify  => Configure['nrsysmond-config-license']
     }
-}
+
+    if $::newrelic::server::install::ensure != 'absent' {
+        exec { 'nrsysmond-config-license':
+            command     => "nrsysmond-config --set license_key=${license_key}",
+            path        => ['/bin', '/usr/bin', '/usr/sbin'],
+            notify      => Service['newrelic-sysmond'],
+            refreshonly => true,
+            subscripe   => Package['newrelic-sysmod']
+        }
+        Package['newrelic-sysmond'] -> Exec['nrsysmond-config-license']
+    }
